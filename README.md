aea29a30a914f596bbedd549e53fac21b8de54ce
Melvin Finn <melvinrf@Outlook.com>
2021-04-03 15:45:20 -0500

    I'm done with this week's assignment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pwordgenerator_mf
+
+In this week's homework, we were tasked with creating a password generator with specific conditions.
--- script.js
+++ script.js
@@ -2,44 +2,39 @@
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
-//global vars
-var charMax = 128;
-var charMin = 8;
-let lowerCase = "abcdefghijklmnopqrstuvwxyz"
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let numberSet = "0123456789"
-let symbols = "!@#$%^&*_+"
-
 // Write password to the #password input
 function writePassword() {
-
+  password = "";
   var numberLimit = prompt("How long do you want your password to be?");
-  if (numberLimit < 8) {
-    alert("Password must be at least 8 characters long.");
-  } else if
-  (numberLimit > 128) {
-    alert("Password cannot exceed 128 characters.");
-  }
+  if (numberLimit < 8 || numberLimit > 128) {
+      alert("Password must be at least 8 characters long and cannot exceed 128 characters.");
+      return;
+    }
 
-  var askNumeric = confirm("Would you like to include numbers?", "Click ok or type no");
+  var askNumeric = confirm("Would you like to include numbers?", "Click ok  for yes and cancel for no");
   var askLowercase = confirm("Would you like to include lower case characters?");
   var askUppercase = confirm("Would you like to include upper case characters?");
   var askSpecialChars = confirm("Would you like to include special characters in your password?")
+
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  let numberSet = "0123456789"
+  let symbols = "!@#$%^&*_+"
+
+  var passwordCharacters = "";
+  if (askNumeric === true) var passwordCharacters = passwordCharacters.concat(numberSet);
+  if (askLowercase === true) var passwordCharacters = passwordCharacters.concat(lowerCase);
+  if (askUppercase === true) var passwordCharacters = passwordCharacters.concat(upperCase);
+  if (askSpecialChars === true) var passwordCharacters = passwordCharacters.concat(symbols);
   
-  for (let i = 0; i < numberLimit; i++) {
-    var password = '';
-    let number = Math.floor(Math.random() * lowerCase.length + upperCase.length + numberSet.length
-    + symbols.length);
-    if (askNumeric === true) password = password.concat(numberSet);
-    if (askLowercase === true) password = password.concat(lowerCase);
-    if (askUppercase === true) password = password.concat(upperCase);
-    if (askSpecialChars === true) password = password.concat(symbols);
-    password += lowerCase.substring + upperCase.substring
-    + numberSet.substring+ symbols.substring(1, number + 1);
+  for (i = 0; i < numberLimit; i++) {
+    let number = Math.floor(Math.random() * passwordCharacters.length);
+    password += passwordCharacters.substring(number, number + 1).trim();
   }
+
   passwordText.value = password;
 
-  console.log(number)
+  console.log(passwordCharacters)
   console.log(typeof password)
   console.log(password);
 }  
